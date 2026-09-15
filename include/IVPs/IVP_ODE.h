c4b7ca670d9c5853e1e11bb918f692603312a354
José Miguel Mantas Ruiz <jmmantas@ugr.es>
2026-09-15 13:50:02 +0200

    IVP_ODE.h modified to enable the easy integration of VSSBDF solvers

--- include/IVPs/IVP_ODE.h
+++ include/IVPs/IVP_ODE.h
@@ -38,6 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <sstream>
 #include <iomanip>
 
+
 using namespace std;
 
 //*****************************************************************
@@ -48,7 +49,9 @@ class IVP_ODE {
 protected:
   int neqn; // number of equations
   string IVP_name; // Name of the IVP model
-  int nnz; //number of non-zero elements in the Jacobian for the stiff term G+H
+  int nnz; //number of non-zero elements in the Jacobian for the stiff terms
+  int nnz_feval; //number of non-zero elements in the Jacobian of the full rhs
+
  
 
   //***************************************************
@@ -104,26 +107,17 @@ public:
 
 
   //***************************************************
-  // Get the number of nonzero elements in Jacobian matrix
+  // Get the number of nonzero elements in the Jacobian matrix
+  // of the stiff terms in the IVP_ODE
   inline int get_nnz() {return nnz;}
   //***************************************************                                              
 
-
   //***************************************************
-  // Init a CSR Sparse neqn x neqn LIS matrix
-  //******************************************************
-  inline void init_CSR_LIS_matrix(const int splitting_type, LIS_MATRIX *A) 
-  {
-    LIS_INT * ptr_A, * index_A;
-    LIS_SCALAR* value_A;
-    lis_matrix_create(0, A);
-    lis_matrix_set_size(*A, 0, neqn);
-    lis_matrix_malloc_csr(neqn, nnz, &ptr_A, &index_A, &value_A);
-    lis_matrix_set_csr(nnz,ptr_A,index_A,value_A,*A);
-    lis_matrix_assemble(*A);
-  }
+  // Get the number of nonzero elements in the Jacobian matrix 
+  // of the full RHS of the ODE system feval
+  inline int get_nnz_feval() {return nnz_feval;}
   //***************************************************                                              
-
+                                            
  
   //******************************************************
   // Initialize the stage vector Y0 with neqn components
@@ -143,8 +137,19 @@ public:
     cblas_daxpy(neqn,1.0,DY_tmp,1,DY,1);
     F3 (t, Y, DY_tmp);
     cblas_daxpy(neqn,1.0,DY_tmp,1,DY,1);
-
   }
+
+  //***************************************************
+  //vector system function for the full stiff terms: G 
+  // DY=G(t,Y)=F1(t,Y)+F2(t,Y)
+  //***************************************************
+  inline void G(const double t, const double *Y, double *DY)
+  { double DY_tmp[neqn];
+    F1 (t, Y, DY);
+    F2 (t, Y, DY_tmp);
+    cblas_daxpy(neqn,1.0,DY_tmp,1,DY,1);
+  }
+
 
   //******************************************************
   //vector system function for the 1st stiff term DY=F1(t,Y)
@@ -163,6 +168,7 @@ public:
 
 
 
+  
   //******************************************************
   // Compute matrix I-a*J1-b*J2 where J1= Jacobian of the 1st stiff Term 
   // J2= Jacobian of the 2nd stiff Term in the IVP_ODE
@@ -171,6 +177,11 @@ public:
                                     double *Y, LIS_MATRIX As)=0;
   //******************************************************
   
+
+
+  
+
+
 
   ///******************************************************
   // Compute matrix  I-a*J1-b*J2, where J1 and J2 are the 
@@ -218,6 +229,48 @@ public:
 
 //******************************************************
 
+///******************************************************
+// Compute matrix  A=I-a*Jf, where Jf= Forward Difference Approximation of
+// Jacobian of the function feval defining full rhs of the IVP_ODE
+//******************************************************       
+void Compute_feval_Matrix_FD(const double t, const double a, double *Y, LIS_MATRIX As)
+  { 
+    double * DY0=new double[neqn];
+    double * DYF=new double[neqn];
+    LIS_INT *ptr=As->ptr;
+    LIS_INT *index=As->index;
+    LIS_SCALAR *value=As->value;
+    LIS_INT  row=0, k;
+    init_row_insertion(ptr, k, row);
+    double Aij;
+    const double uround=1.0e-15;
+    double ysafe[neqn];
+    int nonzeros=0;
+    feval(t, Y, DY0);
+    cblas_dcopy(neqn, Y,1, ysafe, 1);
+    for(int i=0;i<neqn;i++){
+      for(int j=0;j<neqn;j++){
+        const double delta=sqrt(uround*max(1.e-5,abs(ysafe[j])));
+        Y[j]=ysafe[j]+delta;
+        feval(t, Y, DYF);
+        Y[j]=ysafe[j];
+        Aij=-a*(DYF[i]-DY0[i])/delta;
+        if (i==j) Aij+=1.0;
+        if (fabs(Aij)>uround){  
+          new_entry(index, value, k, j, Aij);
+          nonzeros++;
+        } 
+      } 
+      next_row(ptr, k, row);
+  
+    }  
+    cout<<"............ neqn="<<neqn<<"    nonzeros="<<nonzeros<<endl;
+    delete[] DY0;
+    delete[] DYF;    
+}
+
+
+
 
 
 //******************************************************
@@ -233,3 +286,5 @@ virtual void save_vector(const std::string& filename, const double *Y)=0;
 
 
 #endif
+
+
